c17c74bce0a57d3e015b2ba69523110e96c22595
zhouxianggen <zhouxianggen@gmail.com>
2013-09-17 09:42:46 +0800

    f

--- src/main/java/cn/uc/udac/zjj/main/StormMain.java
+++ src/main/java/cn/uc/udac/zjj/main/StormMain.java
@@ -18,15 +18,15 @@ public class StormMain {
 	static public void main(String[] args) {
 		TopologyBuilder builder = new TopologyBuilder();
 
-		builder.setSpout("s_log", new SpoutLog(), 16);
-		builder.setBolt("b_sn_date_site_pv", new BoltSnDateSitePv(), 1).shuffleGrouping("s_log");
-		builder.setBolt("b_date_site_pv", new BoltDateSitePv(), 1).shuffleGrouping("s_log");
-		builder.setBolt("b_date_url_pv", new BoltDateUrlPv(), 4).shuffleGrouping("s_log");
-		builder.setBolt("b_crawler", new BoltCrawler(), 64).shuffleGrouping("b_date_url_pv");
-		builder.setBolt("b_parser", new BoltParser(), 1).shuffleGrouping("b_crawler");
+		builder.setSpout("s_log", new SpoutLog(), 32);
+		builder.setBolt("b_sn_date_site_pv", new BoltSnDateSitePv(), 4).shuffleGrouping("s_log");
+		builder.setBolt("b_date_site_pv", new BoltDateSitePv(), 4).shuffleGrouping("s_log");
+		builder.setBolt("b_date_url_pv", new BoltDateUrlPv(), 16).shuffleGrouping("s_log");
+		builder.setBolt("b_crawler", new BoltCrawler(), 128).shuffleGrouping("b_date_url_pv");
+		builder.setBolt("b_parser", new BoltParser(), 4).shuffleGrouping("b_crawler");
 		
 		Config conf = new Config();
-		conf.setNumWorkers(5);
+		conf.setNumWorkers(16);
 		Map myconf = Utils.findAndReadConfigFile("zjj.yaml");
 		conf.putAll(myconf);
 
