c3487349a160cd4b42fa2ea8055d401b03b81f94
zhouxianggen <zhouxianggen@gmail.com>
2013-11-14 10:43:49 +0800

    f

---
+++ src/main/java/cn/uc/udac/zjj/bolts/BoltTimeSite.java
@@ -0,0 +1,100 @@
+/*
+ * BoltTimeSite
+ * 
+ * 1.0 记录每段时间上的site分布
+ *
+ * zhouxg@ucweb.com 
+ */
+
+
+package cn.uc.udac.zjj.bolts;
+
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import redis.clients.jedis.Jedis;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Tuple;
+
+
+public class BoltTimeSite extends BaseBasicBolt {
+	
+	static public Logger LOG = Logger.getLogger(BoltCitySite.class);
+	private Jedis[] _arrRedisServer;
+	private Map _conf;
+	private int _count = 0;
+
+	private void init(Map conf) {
+		try {
+			List<String> hosts = (List<String>)conf.get("time_site_redis_hosts");
+			int port = ( (Long)conf.get("redis_port") ).intValue();
+			
+			LOG.info(String.format("BoltTimeSite.init, hosts=%s, port=%d", StringUtils.join(hosts, ","), 
+					port));
+			
+			_arrRedisServer = new Jedis[hosts.size()];
+			
+			for (int i = 0; i < hosts.size(); ++i) {
+				_arrRedisServer[i] = new Jedis(hosts.get(i), port);
+			}
+		} catch (Exception e) {
+			LOG.info("BoltTimeSite.init.exception:", e);
+		}
+	}
+	
+	@Override
+	public void prepare(Map conf, TopologyContext context) {
+		_conf = conf;
+		init(_conf);
+    }
+    
+	private int hash(String key) {
+		int h = 0;
+		
+		for (int i = 0; i < key.length(); ++i) {
+			h += key.codePointAt(i);
+		}
+		
+		return h % _arrRedisServer.length;
+	}
+	
+    @Override
+	public void execute(Tuple input, BasicOutputCollector collector) {
+    	try {
+    		String time = input.getString(0);
+	    	String url = input.getString(5);
+	    	Date tmp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(time);
+	    	String timeStamp = new SimpleDateFormat("yyyy-MM-dd-HH").format(tmp);
+	    	String site = new URL(url).getHost();
+	    	String key = timeStamp;
+	    	int h = hash(key);
+	    	int seconds = 24 * 3600;
+	    	
+	    	if (++_count % 100 == 0) {
+	    		LOG.info(String.format("BoltTimeSite %d: time=%s site=%s", _count, time, site));
+	    		LOG.info(String.format("BoltTimeSite %d: key=%s h=%d", _count, key, h));
+	    	}
+	    	
+	    	_arrRedisServer[h].zincrby(key, 1, site);
+	    	_arrRedisServer[h].expire(key, seconds);
+		} catch (Exception e) {
+			LOG.info("BoltTimeSite.execute.exception:", e);
+			init(_conf);
+		}
+	}
+
+    @Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+	}
+
+}
